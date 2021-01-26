691fbed8237608264833c282368e0c976628d23c
tider <tiderzheng@gmail.com>
2021-01-26 23:30:16 +0800

    test

--- src/practice_one/hello.java
+++ src/practice_one/hello.java
@@ -2,7 +2,7 @@ package practice_one;
 
 public class hello {
     public static void main(String args[]){
-        System.out.println("Hello world");
+        System.out.println("Hello world github");
 
     }
 }
